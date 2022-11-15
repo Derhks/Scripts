c80b107143cc84ac75fe23375cf0624026c8b976
derhk <derhks@gmail.com>
2022-11-14 20:37:45 -0500

    feat: Seleccionar las rutas de las carpetas

--- filtering_images_gui.py
+++ filtering_images_gui.py
@@ -1,6 +1,38 @@
 import tkinter as tk
+from tkinter import filedialog
 
 from utils import filtering_images, move_folder_image, validate_folder_exist
+
+
+new_directory_path = ""
+actual_directory_path = ""
+
+
+# call function when we click on entry box
+def width_click(*args):
+    min_width.delete(0, 'end')
+
+
+def height_click(*args):
+    min_height.delete(0, 'end')
+
+
+def ask_actual_directory() -> None:
+    """
+    Obtiene la ruta de la actual carpeta en la cual se van a guardar las
+    imágenes
+    """
+    global actual_directory_path
+    actual_directory_path = filedialog.askdirectory()
+
+
+def ask_new_directory() -> None:
+    """
+    Obtiene la ruta de la nueva carpeta en la cual se van a guardar las
+    imágenes
+    """
+    global new_directory_path
+    new_directory_path = filedialog.askdirectory()
 
 
 def clear() -> None:
@@ -9,15 +41,13 @@ def clear() -> None:
     """
     min_width.delete(0, tk.END)
     min_height.delete(0, tk.END)
-    actual_directory.delete(0, tk.END)
-    new_directory.delete(0, tk.END)
 
     ...
 
 
 def main() -> None:
-    actual_dir = actual_directory.get()
-    new_dir = new_directory.get()
+    actual_dir = actual_directory_path
+    new_dir = new_directory_path
     image_min_width = int(min_width.get())
     image_min_height = int(min_height.get())
 
@@ -48,28 +78,26 @@ window.resizable(width=False, height=False)
 
 width_entry = tk.Frame(master=window)
 min_width = tk.Entry(master=width_entry, width=20, borderwidth=5)
-label_min_width = tk.Label(master=window, text="Anchura de la imagen")
+min_width.insert(0, "Anchura de la imagen")
+min_width.bind("<Button-1>", width_click)
 
 height_entry = tk.Frame(master=window)
 min_height = tk.Entry(master=height_entry, width=20, borderwidth=5)
-label_min_height = tk.Label(master=window, text="Altura de la imagen")
+min_height.insert(0, "Altura de la imagen")
+min_height.bind("<Button-1>", height_click)
 
-actual_directory_entry = tk.Frame(master=window)
-actual_directory = tk.Entry(
-    master=actual_directory_entry,
-    width=20,
-    borderwidth=5
-)
-label_actual_directory = tk.Label(
+btn_ask_actual_dir = tk.Button(
     master=window,
-    text="Ruta de la carpeta actual de las imágenes"
+    text="Actual Directorio",
+    command=lambda:ask_actual_directory(),
+    width=15
 )
 
-new_directory_entry = tk.Frame(master=window)
-new_directory = tk.Entry(master=new_directory_entry, width=20, borderwidth=5)
-label_new_directory = tk.Label(
+btn_new_actual_dir = tk.Button(
     master=window,
-    text="Ruta de la nueva carpeta de las imágenes"
+    text="Nuevo Directorio",
+    command=lambda:ask_new_directory(),
+    width=15
 )
 
 btn_filter = tk.Button(
@@ -77,6 +105,7 @@ btn_filter = tk.Button(
     text="Filtrar",
     command=main
 )
+
 btn_clear = tk.Button(
     master=window,
     text="Limpiar",
@@ -87,21 +116,16 @@ label_result = tk.Label(master=window, text="Esperando...")
 
 width_entry.grid(row=0, column=0, padx=15, pady=15)
 min_width.grid(row=0, column=0, sticky="e")
-label_min_width.grid(row=0, column=1, sticky="w")
 
 height_entry.grid(row=1, column=0)
 min_height.grid(row=1, column=0, sticky="e")
-label_min_height.grid(row=1, column=1, sticky="w")
 
-actual_directory_entry.grid(row=2, column=0, pady=15)
-actual_directory.grid(row=2, column=0, sticky="e")
-label_actual_directory.grid(row=2, column=1, sticky="w")
+btn_ask_actual_dir.grid(row=2, column=0, padx=15, pady=10)
 
-new_directory_entry.grid(row=3, column=0)
-new_directory.grid(row=3, column=0, sticky="e")
-label_new_directory.grid(row=3, column=1, sticky="w")
+btn_new_actual_dir.grid(row=3, column=0, padx=15, pady=10)
 
 btn_filter.grid(row=4, column=2, pady=10)
+
 btn_clear.grid(row=4, column=3, pady=10)
 
 label_result.grid(row=6, column=3, padx=10, sticky="w")
